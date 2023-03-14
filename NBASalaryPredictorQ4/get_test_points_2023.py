b6b32a44bbf312e5e32ee190ffcf45e3a0dc6586
KiriSchrieffer <100171031+KiriSchrieffer@users.noreply.github.com>
2023-03-13 21:43:34 -0700

    Add files via upload

---
+++ NBASalaryPredictorQ4/get_test_points_2023.py
@@ -0,0 +1,29 @@
+# Author: Qingyuan Zhang
+# This is a crawler file to get test data of 3-point field goal,
+# assist and points per game of players.
+# This program will output a csv file, ANSI encoding.
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+
+wb = Workbook()
+ws = wb.active
+url = "https://www.basketball-reference.com/leagues/NBA_2023_totals.html"
+response = requests.get(url)
+content = response.content
+soup = BeautifulSoup(content, "html.parser")
+headers = soup.find_all("thead")
+fg3_pct = soup.find_all("td", attrs={"data-stat": "fg3_pct"})
+ast = soup.find_all("td", attrs={"data-stat": "ast"})
+pts = soup.find_all("td", attrs={"data-stat": "pts"})
+player = soup.find_all("td", attrs={"data-stat": "player"})
+games = soup.find_all("td", attrs={"data-stat": "g"})
+lis = ["name", "fg3_pct", "ast", "pts"]
+ws.append(lis)
+for i in range(0, len(pts)):
+    if fg3_pct[i].text != "":
+        liss = [player[i].text, float(fg3_pct[i].text) * 100, int(ast[i].text)
+                / int(games[i].text), int(pts[i].text) / int(games[i].text)]
+        ws.append(liss)
+wb.save("test_points_2023.csv")
---
+++ NBASalaryPredictorQ4/get_test_salary_2023.py
@@ -0,0 +1,34 @@
+# Author: Qingyuan Zhang
+# This is a crawler file to get test data of salaries of players.
+# This program will output a csv file, ANSI encoding.
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.active
+
+lis = []
+
+for num in range(1, 14):
+    url = f"http://www.espn.com/nba/salaries/_/page/{num}"
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, "html.parser")
+
+    names = soup.find_all("td")
+    for name in names:
+        lis.append(name.text)
+
+for i in range(0, len(lis), 4):
+    if i == 0:
+        liss = []
+        liss.append(lis[i + 1].split(',')[0])
+        liss.append(lis[i + 3])
+        ws.append(liss)
+    if lis[i + 3][0] == '$':
+        liss = []
+        liss.append(lis[i + 1].split(',')[0])
+        liss.append(lis[i + 3][1:].replace(",", ""))
+        ws.append(liss)
+wb.save("test_salary_2023.csv")
